50eb69d5c0d2a34e024dbab799253ffa7b5dadd4
PasararAndrey <56974924+PasararAndrey@users.noreply.github.com>
2023-01-27 16:42:59 +0200

    Update get_data.sql

--- get_data.sql
+++ get_data.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE FUNCTION get_data(v_real_name VARCHAR, v_user_name VARCHAR, v_token VARCHAR)
+CREATE OR REPLACE FUNCTION get_data(human_name VARCHAR, v_user_name VARCHAR, v_token VARCHAR)
 RETURNS TABLE
-    (p_id INTEGER, name VARCHAR, bd DATE)
+(s_id INTEGER, stud_name VARCHAR, kurs INTEGER)
 AS $$
 DECLARE
-	str VARCHAR;
+str VARCHAR;
 BEGIN
 	CALL sso_control(v_user_name,v_token);
-	str := 'SELECT p_id, name, bd from public.human where name = $1';
-    	RAISE NOTICE 'Query=%',str;
-	RETURN QUERY EXECUTE str USING v_real_name
+	str := 'SELECT s_id, stud_name, kurs from public.student where name = $1';
+	RAISE NOTICE 'Query=%',str;
+	RETURN QUERY EXECUTE str USING human_name;
 END;
 $$ LANGUAGE plpgsql;
